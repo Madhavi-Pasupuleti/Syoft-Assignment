b359073db2073fc6318cb45fb32b7893883c4fa9
Madhavi-Pasupuleti <95956493+Madhavi-Pasupuleti@users.noreply.github.com>
2022-08-02 18:06:44 +0530

    installed cors

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
+const cors = require("cors")
 app.use(express.json())
+app.use(cors())
 
 const connect = require("./configs/db");
 const {register, login} = require("./controllers/auth.controller")
